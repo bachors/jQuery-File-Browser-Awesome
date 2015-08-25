6f8803fdb4dd9f262a219a2e9c518797e37fe68f
Bachors <bachor.can@gmail.com>
2015-08-26 00:46:10 +0700

    Update fba.js

--- js/fba.js
+++ js/fba.js
@@ -8,6 +8,9 @@
 // Base_URL scandir.php
 var baseurl = 'http://localhost/fba/scandir.php';
 
+// Folder yang akan discan
+var browser = 'download';
+
 /* Testing di localhost */
 var fix = /\\/;
 
@@ -17,12 +20,19 @@ var fix = /\//;
 
 var value = getParameterByName('dir');
 if(value != ''){
-	ibc_fba(baseurl,sub=value,fix);
+	if(value.indexOf('.') === -1){
+		ibc_fba(baseurl,sub=value,fix,browser);
+	}else{
+		var valust = value.substring(value.lastIndexOf('/') + 1),
+		gehu = value.replace('/'+valust, "");
+		ibc_fba(baseurl,sub=gehu,fix,browser);
+		ibc_fba_file(baseurl,browser+value);
+	}
 }else{
-	ibc_fba(baseurl,sub="",fix)
+	ibc_fba(baseurl,sub="",fix,browser)
 }
 
-function ibc_fba(baseurl,sub,fix) {
+function ibc_fba(baseurl,sub,fix,browser) {
 	$.ajax( {
 		url:baseurl+'?sub='+sub,
 		crossDomain:true,
@@ -32,7 +42,7 @@ function ibc_fba(baseurl,sub,fix) {
 		r+="<table>";
 		r+='<thead><tr><th class="name">Name</th><th class="size">Size</th><th class="modif">Last Modified</th></tr></thead><tbody>';
 		if(data.back!="") {
-			r+='<tr><td colspan="3" class="root"><i class="bsub fa fa-reply" data-bsub="'+data.back.replace(fix,"")+'"></i> '+data.root+"</td></tr>"
+			r+='<tr><td colspan="3" class="root"><i class="bsub fa fa-reply" data-bsub="'+data.back.replace(fix,"")+'" title="Up"></i> '+data.root+"<a href='http://ibacor.com/labs/jquery-file-browser-awesome' title='jQuery File Browser Awesome' style='float:right' target='_BLANK'><i class='fa fa-info-circle'></i></a></td></tr>"
 		}
 		$.each(data.items,function(i,item) {
 			var size=ibc_ukurana(data.items[i].size);
@@ -47,32 +57,32 @@ function ibc_fba(baseurl,sub,fix) {
 					case"js":
 					case"css":
 					case"txt":
-					r+='<tr><td class="name"><span class="rfile fa" data-rfile="'+data.items[i].path+'"><i class="fa fa-file-text"></i> '+data.items[i].name+'</span></td><td class="size">'+size+'</td><td class="modif">'+data.items[i].modif+'</td></tr>';
+					r+='<tr><td class="name"><span class="rfile fa" data-rfile="'+data.items[i].path+'"><i class="fa fa-file-text-o"></i> '+data.items[i].name+'</span></td><td class="size">'+size+'</td><td class="modif">'+data.items[i].modif+'</td></tr>';
 					break;
 					// List download file
 					default:
-					r+='<tr><td class="name"><a href="'+data.items[i].link+data.items[i].path+'" target="_blank"><i class="fa fa-archive"></i> '+data.items[i].name+'</a></td><td class="size">'+size+'</td><td class="modif">'+data.items[i].modif+'</td></tr>';
+					r+='<tr><td class="name"><a href="'+data.items[i].link+data.items[i].path+'" target="_blank"><i class="fa fa-cloud-download"></i> '+data.items[i].name+'</a></td><td class="size">'+size+'</td><td class="modif">'+data.items[i].modif+'</td></tr>';
 				}
 			}
 		});
-		r+='<tr><td colspan="3" style="text-align:center;font-size:8px;padding-top:30px">'+unescape("%3C%61%20%68%72%65%66%3D%22%68%74%74%70%3A%2F%2F%69%62%61%63%6F%72%2E%63%6F%6D%2F%6C%61%62%73%2F%6A%71%75%65%72%79%2D%66%69%6C%65%2D%62%72%6F%77%73%65%72%2D%61%77%65%73%6F%6D%65%2F%22%20%74%61%72%67%65%74%3D%22%5F%42%4C%41%4E%4B%22%3E%46%69%6C%65%20%42%72%6F%77%73%65%72%20%41%77%65%73%6F%6D%65%3C%2F%61%3E")+'</td></tr>';
 		r+="</tbody></table>";
 		$(".ibc_fba").html(r);
 		$(".sub").click(function() {
 			var t=$(this).data("sub");
-			ibc_fba(baseurl,t,fix);
+			ibc_fba(baseurl,t,fix,browser);
 			window.history.pushState(null, null, "?dir="+t);
 			return false
 		});
 		$(".bsub").click(function() {
 			var t=$(this).data("bsub");
-			ibc_fba(baseurl,t,fix);
+			ibc_fba(baseurl,t,fix,browser);
 			window.history.pushState(null, null, "?dir="+t);
 			return false
 		});
 		$(".rfile").click(function() {
 			var file=$(this).data("rfile");
 			ibc_fba_file(baseurl,file);
+			window.history.pushState(null, null, "?dir="+file.replace(browser, ""));
 			return false
 		});
 	});
